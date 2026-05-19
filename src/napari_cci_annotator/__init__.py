41a8e3fb890f5f570da69f9157af92accaf03bdc
Noema <bosio.noe@gmail.com>
2026-05-19 18:00:26 +0200

    fix: decouple package import from napari widget initialization

--- src/napari_cci_annotator/__init__.py
+++ src/napari_cci_annotator/__init__.py
@@ -3,12 +3,4 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import CciAnnotatorQWidget
-from ._writer import write_multiple, write_single_image
-
-__all__ = (
-    "CciAnnotatorQWidget",
-    "__version__",
-)
+__all__ = ("__version__",)
