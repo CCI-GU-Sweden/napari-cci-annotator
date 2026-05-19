c1a4947f02db28b0fadedca016e0af9ed36bba6b
Noema <bosio.noe@gmail.com>
2026-05-19 17:50:21 +0200

    Modified workflow for build and deploy. Version is now fetched from git tag.

--- src/napari_cci_annotator/__init__.py
+++ src/napari_cci_annotator/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "0.7.1"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
@@ -7,4 +10,5 @@ from ._writer import write_multiple, write_single_image
 
 __all__ = (
     "CciAnnotatorQWidget",
+    "__version__",
 )
